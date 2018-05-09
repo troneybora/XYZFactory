af4f41edb4e53386cf9cbd9feaefbaa2d49f9b16
Troney <troneybora@hotmail.com>
2018-05-10 02:05:22 +0530

    First Commit

---
+++ src/xyzfactory/XYZFactory.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package xyzfactory;
+
+/**
+ *
+ * @author Pramod
+ */
+public class XYZFactory {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        new XYZ().setVisible(true); //Making the object of Class XYZ and setting the visible as True
+    }
+    
+}
